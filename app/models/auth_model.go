f31ea91102203b3b8cb2e9ad6605a7b62b76795f
Eeap <kdg97811@naver.com>
2023-12-05 16:37:31 +0900

    refactor: del auth model

--- app/models/auth_model.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// SignUp struct to describe register a new user.
-
-// UserData struct to describe login user.
-type UserData struct {
-	Email    string `json:"email" validate:"required,email,lte=255"`
-	Password string `json:"password" validate:"required,lte=255"`
-}
